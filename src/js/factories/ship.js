dae21a1b3bccaaf59763661ef2808c588788b92a
dipto0321 <diptokmk47@gmail.com>
2019-03-13 09:32:13 +0600

    Ship factory build
    - Create hit() for calculate the number of hits
    - Create isSunk() for checking the ship is sunk or not

--- src/js/factories/ship.js
+++ src/js/factories/ship.js
@@ -1,5 +1,12 @@
 const Ship = ((length) => ({
   length,
+  hits: 0,
+  hit() {
+    this.isSunk() ? undefined : this.hits += 1;
+  },
+  isSunk() {
+    return this.hits >= this.length;
+  }
 }));
 
 module.exports = Ship;
--- src/js/test/ship.test.js
+++ src/js/test/ship.test.js
@@ -6,7 +6,26 @@ describe('Ship', () => {
     ship = Ship(5);
   })
   test('ship has a length property', () => {
-    // const ship = Ship(5);
     expect(ship.length).toBe(5);
   });
+
+
+  describe("hit", () => {
+    test('hit() should increment the number of hits oth the ship', () => {
+      ship.hit();
+      expect(ship.hits).toBe(1);
+    });
+    test('hit() should not increament hits if ship is already sunk', () => {
+      const sub = Ship(1);
+      sub.hit();
+      sub.hit();
+      expect(sub.hits).toBe(1);
+    });
+  });
+  test('isSunk() should return false if the number of hits is less then the length of the ship', () => {
+    ship.hit();
+    ship.hit();
+    expect(ship.isSunk()).toBeFalsy();
+  });
+
 });
