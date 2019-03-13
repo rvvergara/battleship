a72246d381e105f2fdd83baeaa1333e6c5247754
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-03-13 10:42:28 +0800

    Initial project setup and first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Battleship
+
+## A Strategy Game built using JavaScript
+
+### by [Ryan](https://github.com/rvvergara) and [Dipto](https://github.com/dipto0321)
+
+### [Play Game Here](#)
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/js/index.js',
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    contentBase: './dist',
+  },
+  module: {
+    rules: [{
+        test: /\.css$/,
+        loader: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+    ],
+  },
+}
