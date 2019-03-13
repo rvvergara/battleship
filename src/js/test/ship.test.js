047a2e047fd32bb8e3c0461aaedbf5abd0fae519
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-03-13 11:01:42 +0800

    Add beforeEach hook to ship test

--- src/js/test/ship.test.js
+++ src/js/test/ship.test.js
@@ -1,8 +1,12 @@
 const Ship = require('../factories/ship');
 
 describe('Ship', () => {
+  let ship;
+  beforeEach(() => {
+    ship = Ship(5);
+  })
   test('ship has a length property', () => {
-    const ship = Ship(5);
+    // const ship = Ship(5);
     expect(ship.length).toBe(5);
   });
-})
+});
