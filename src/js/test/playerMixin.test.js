e65ef2a5901aabbe1c8c69d5af380020df9a8dca
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-03-14 14:02:57 +0800

    Refactor test for computerMakeChoice in mixin

--- src/js/test/playerMixin.test.js
+++ src/js/test/playerMixin.test.js
@@ -1,17 +1,15 @@
 const {
   computerMakeChoice,
   humanMakeChoice
-} = require('../mixins/playerMixin');
+} = require("../mixins/playerMixin");
 
-describe('playerMixin', () => {
-  describe('computerMakeChoice', () => {
-    test('computerMakeChoice returns a valid number', () => {
+describe("playerMixin", () => {
+  describe("computerMakeChoice", () => {
+    test("computerMakeChoice returns a valid number", () => {
       const arr = [1, 2, 3, 4, 5];
-      expect(computerMakeChoice(arr)).toMatch(/\d/);
+      expect(computerMakeChoice(arr)).toBeLessThanOrEqual(5);
     });
   });
 
-  describe('humanMakeChoice', () => {
-
-  });
+  describe("humanMakeChoice", () => {});
 });
