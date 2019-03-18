1399cc74603159accb022485a1a027087dbcf360
dipto0321 <diptokmk47@gmail.com>
2019-03-18 10:09:08 +0600

    Extract computer turns from game turn
    To make game turn function single responsibity

--- src/js/factories/gameBoard.js
+++ src/js/factories/gameBoard.js
@@ -1,14 +1,17 @@
-const GameBoard = (ships => ({
+const GameBoard = ships => ({
   grid: new Array(100),
   ships,
   setShipPosition(ship, index, orientation) {
-    const limit = orientation === 'horizontal' ? index + ship.length : index + 10 * (ship.length);
+    const limit =
+      orientation === "horizontal"
+        ? index + ship.length
+        : index + 10 * ship.length;
     ship.position = ship.position || [];
-    for (let i = index; i < limit;) {
+    for (let i = index; i < limit; ) {
       if (this.grid[i] === undefined) {
         ship.position.push(i);
         this.grid[i] = i;
-        orientation === 'horizontal' ? i += 1 : i += 10;
+        orientation === "horizontal" ? (i += 1) : (i += 10);
       } else {
         this.grid[i - 1] = undefined;
         ship.position.pop();
@@ -20,7 +23,7 @@ const GameBoard = (ships => ({
     // Determine if the index in grid is occupied
     if (this.grid[index] !== undefined) {
       // If yes then determine the ship that occupies it
-      Object.keys(ships).forEach((key) => {
+      Object.keys(ships).forEach(key => {
         if (ships[key].position.includes(index)) ships[key][method]();
       });
     } else {
@@ -31,13 +34,13 @@ const GameBoard = (ships => ({
   allShipsSunk(isSunkMethod, pname) {
     // return Object.keys(ships).every(key => ships[key][isSunkMethod]());
 
-    const shipSunkArray = Object.keys(ships).map((key) => {
+    const shipSunkArray = Object.keys(ships).map(key => {
       // console.log(`${pname} ${key} hits: `, ships[key].hits);
       return ships[key][isSunkMethod]();
     });
     // console.log(shipSunkArray);
     return shipSunkArray.every(bool => bool === true);
-  },
-}));
+  }
+});
 
 module.exports = GameBoard;
--- src/js/mainGame.js
+++ src/js/mainGame.js
@@ -1,9 +1,9 @@
-const Ship = require('./factories/ship');
-const GameBoard = require('./factories/gameBoard');
-const Player = require('./factories/player');
+const Ship = require("./factories/ship");
+const GameBoard = require("./factories/gameBoard");
+const Player = require("./factories/player");
 const {
-  computerMakeChoice,
-} = require('./mixins/playerMixin');
+  computerMakeChoice
+} = require("./mixins/playerMixin");
 
 const mainGame = () => {
   const battleShipObjs = ((Ship, GameBoard, Player) => {
@@ -11,71 +11,77 @@ const mainGame = () => {
       cruiser1: Ship(1),
       frigate1: Ship(2),
       destroyer1: Ship(3),
-      carrier: Ship(4),
+      carrier: Ship(4)
     });
     const computerFleet = Object.assign({
       cruiser1: Ship(1),
       frigate1: Ship(2),
       destroyer1: Ship(3),
-      carrier: Ship(4),
+      carrier: Ship(4)
     });
     // 2. Create two gameBoards
     const humanBoard = GameBoard(humanFleet);
     const computerBoard = GameBoard(computerFleet);
     // 3. Create two players
-    const human = Object.assign(Player(computerBoard, 'receiveAttack', 'hit'));
+    const human = Object.assign(Player(computerBoard, "receiveAttack", "hit"));
 
-    const computer = Object.assign(Player(humanBoard, 'receiveAttack', 'hit'), {
-      makeChoice: computerMakeChoice,
+    const computer = Object.assign(Player(humanBoard, "receiveAttack", "hit"), {
+      makeChoice: computerMakeChoice
     });
 
     // Position ships for human
-    humanBoard.setShipPosition(humanFleet.cruiser1, 0, 'vertical');
-    humanBoard.setShipPosition(humanFleet.frigate1, 2, 'vertical');
-    humanBoard.setShipPosition(humanFleet.destroyer1, 4, 'vertical');
-    humanBoard.setShipPosition(humanFleet.carrier, 6, 'vertical');
+    humanBoard.setShipPosition(humanFleet.cruiser1, 0, "vertical");
+    humanBoard.setShipPosition(humanFleet.frigate1, 2, "vertical");
+    humanBoard.setShipPosition(humanFleet.destroyer1, 4, "vertical");
+    humanBoard.setShipPosition(humanFleet.carrier, 6, "vertical");
 
     // Position ships for computer
-    computerBoard.setShipPosition(computerFleet.cruiser1, 0, 'vertical');
-    computerBoard.setShipPosition(computerFleet.frigate1, 2, 'vertical');
-    computerBoard.setShipPosition(computerFleet.destroyer1, 4, 'vertical');
-    computerBoard.setShipPosition(humanFleet.carrier, 6, 'vertical');
+    computerBoard.setShipPosition(computerFleet.cruiser1, 0, "vertical");
+    computerBoard.setShipPosition(computerFleet.frigate1, 2, "vertical");
+    computerBoard.setShipPosition(computerFleet.destroyer1, 4, "vertical");
+    computerBoard.setShipPosition(humanFleet.carrier, 6, "vertical");
 
     return {
       humanBoard,
       computerBoard,
       human,
-      computer,
+      computer
     };
   })(Ship, GameBoard, Player);
 
-
-  const gameTurn = (index, humanPlayer, computerPlayer, humanBoard, computerBoard) => {
+  const gameTurn = (
+    index,
+    humanPlayer,
+    computerPlayer,
+    humanBoard,
+    computerBoard
+  ) => {
     // humanPlayer turn(index) gets called
     humanPlayer.turn(Number(index.substr(2)));
     // if computer's gameBoard is still alive then computerPlayer turn(computerPlayer.makeChoice()) gets called
-
     // Computer choices
-    const choices = [];
+    const turns = computerTurn(computerPlayer, computerBoard, humanBoard, index);
+    return turns;
+  };
 
-    if (!computerBoard.allShipsSunk('isSunk', 'Computer') && computerBoard.grid[Number(index.substr(2))] === '*') {
-      let choice = computerPlayer.makeChoice(humanBoard.grid);
-      computerPlayer.turn(choice);
-      while (humanBoard.grid[choice] !== '*') {
+  const computerTurn = (player, ownBoard, oponnentBoard, index) => {
+    const choices = [];
+    if (!ownBoard.allShipsSunk('isSunk', 'Computer') && ownBoard.grid[Number(index.substr(2))] === '*') {
+      let choice = player.makeChoice(oponnentBoard.grid);
+      player.turn(choice);
+      while (oponnentBoard.grid[choice] !== '*') {
         choices.push(choice);
-        choice = computerPlayer.makeChoice(humanBoard.grid);
-        computerPlayer.turn(choice);
+        choice = player.makeChoice(oponnentBoard.grid);
+        player.turn(choice);
       }
       choices.push(choice);
-      // 
     }
     return choices;
-    // check if someone has won
   };
 
   return {
     gameTurn,
-    battleShipObjs,
+    battleShipObjs
   };
 };
 
