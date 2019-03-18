16413b45443884555d6d04d1e41c7ce6545edbac
dipto0321 <diptokmk47@gmail.com>
2019-03-18 11:50:16 +0600

    Create initial mockup for gaurd layer div
    - Just try this div according to our conditions

--- src/js/dom/domUtils.js
+++ src/js/dom/domUtils.js
@@ -9,7 +9,8 @@ const {
 
 const container = document.querySelector('.container');
 
-const attackCallBack = (target) => {
+const attackCallBack = (target, boardName) => {
+  guardBox(document.querySelector('.h'));
   // Call gameTurn method
   const index = Number(target.id.substr(2));
   const turns = mainGame().gameTurn(index, human, computer, humanBoard, computerBoard);
@@ -26,17 +27,19 @@ const attackCallBack = (target) => {
     turns.forEach((turn) => {
       const humanSquare = document.getElementById(`h-${turn}`);
       setTimeout(() => changeText(humanSquare, humanBoard, turn), 2000);
+
     });
   }
 };
 
 const changeText = (humanSquare, humanBoard, turn) => {
   humanSquare.innerText = humanBoard.grid[turn];
+  document.querySelector('.gaurd-box').classList.add('invisible');
 };
 
 const createGrid = (num, boardName) => {
   const grid = document.createElement('div');
-  grid.setAttribute('class', 'col-5 mx-3 mt-5');
+  grid.setAttribute('class', `col-5 mx-3 mt-5 ${boardName}`);
 
   for (let i = 0; i < num; i++) {
     grid.appendChild(createRow(num, i, boardName));
@@ -55,7 +58,7 @@ const createRow = (num, rowNum, boardName) => {
     box.setAttribute('id', `${boardName}-${((rowNum * 10) + i)}`);
     box.addEventListener('click', (e) => {
       e.stopPropagation();
-      attackCallBack(e.target);
+      attackCallBack(e.target, boardName);
     }, {
       once: true,
     });
@@ -66,6 +69,12 @@ const createRow = (num, rowNum, boardName) => {
 
 const humanBoardGrid = createGrid(10, 'h');
 const computerBoardGrid = createGrid(10, 'c');
+
+const guardBox = (parent) => {
+  const bigBox = document.createElement('div');
+  bigBox.setAttribute('class', 'bg-secondary position-absolute gaurd-box');
+  parent.appendChild(bigBox);
+}
 
 export {
   createGrid,
