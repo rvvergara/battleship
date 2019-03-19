eba6052fc442c4ddeed34dc6da7336356c2eed3a
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-03-19 13:14:25 +0800

    Fix code for playerMixin

--- src/js/mixins/playerMixin.js
+++ src/js/mixins/playerMixin.js
@@ -7,31 +7,33 @@ const playerMixin = (() => ({
     }
     return index;
   },
+
+  choiceSanitizer(player, opponentBoard, choice, randomGeneratorFn) {
+    const choices = [];
+    // For as long as the choice does not correspond to an * then keep making a choice
+    while (opponentBoard.grid[choice] !== '*') {
+      choices.push(choice);
+      choice = randomGeneratorFn(opponentBoard.grid);
+      player.turn(choice);
+    }
+    choices.push(choice);
+    // If the function goes inside the while loop then choices.length > 0
+    return choices;
+  },
+
   // computerMakeChoice(player, ownBoard, opponentBoard, index)
-  computerMakeChoice(options) {
+  computerMakeChoice(options, randomGeneratorFn, sanitizingFn) {
     const choices = [];
 
     if (options.boardMethod(options.shipMethod) === false && options.ownBoard.grid[options.index] === '*') {
 
-      const choice = options.player.generateRandomNumberFromArray(options.opponentBoard.grid);
+      const choice = randomGeneratorFn(options.opponentBoard.grid);
       options.player.turn(choice);
 
-      choices.concat(choiceSanitizer(options.player, options.opponentBoard, options.choice));
+      choices.concat(sanitizingFn(options.player, options.opponentBoard, choice));
     }
     return choices;
   },
-
 }))();
-
-const choiceSanitizer = (player, opponentBoard, choice) => {
-  const choices = [];
-  while (opponentBoard.grid[choice] !== '*') {
-    choices.push(choice);
-    choice = player.generateRandomNumberFromArray(opponentBoard.grid);
-    player.turn(choice);
-  }
-  choices.push(choice);
-  return choices;
-};
 
 module.exports = playerMixin;
--- src/js/test/playerMixin.test.js
+++ src/js/test/playerMixin.test.js
@@ -1,11 +1,13 @@
 const {
   generateRandomNumberFromArray,
+  choiceSanitizer,
   computerMakeChoice,
 } = require("../mixins/playerMixin");
 
 describe("playerMixin", () => {
-  test('computerMakeChoice returns an array of choices', () => {
-    const options = {
+  let options = {};
+  beforeEach(() => {
+    options = {
       player: {
         generateRandomNumberFromArray,
         turn: function fool(choice) {
@@ -16,7 +18,7 @@ describe("playerMixin", () => {
         grid: ['*', , 2, 3, '*'],
       },
       opponentBoard: {
-        grid: [0, 1, , , , ],
+        grid: [0, 1, '*', , , ],
       },
       index: 4,
       boardMethod: function foo(shipMethod) {
@@ -26,7 +28,17 @@ describe("playerMixin", () => {
         return false;
       },
     };
-    expect(computerMakeChoice(options)).toEqual([0]);
+  });
+
+  test('choiceSanitizer should return an array', () => {
+    const randomGenMocker = () => 2;
+    expect(choiceSanitizer(options.player, options.opponentBoard, 0, randomGenMocker).length).toBe(2);
+  });
+
+  test('computerMakeChoice returns an array of choices', () => {
+    const randomGenMocker = () => 0;
+    const sanitize = (p, b, c) => [2];
+    expect(computerMakeChoice(options, randomGenMocker, sanitize)).toEqual([]);
   });
 
   test("generateRandomNumberFromArray returns a valid number", () => {
