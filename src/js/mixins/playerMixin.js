9d6cdfb6dcd7cab4f87ddc0170212a41e8798858
dipto0321 <diptokmk47@gmail.com>
2019-03-26 10:15:13 +0600

    Remove unnessary comments from playerMixin

--- src/js/mixins/playerMixin.js
+++ src/js/mixins/playerMixin.js
@@ -1,13 +1,11 @@
 const playerMixin = (() => ({
   intelligentChoiceGenerator(arr, player, randomGeneratorFn) {
     const queue = player.shotsRecord.shotsQueue;
-    const choice =
-      queue.length === 0 ? randomGeneratorFn(arr, player) : queue.shift();
+    const choice = queue.length === 0 ? randomGeneratorFn(arr, player) : queue.shift();
     player.shotsRecord.shotsMade.push(choice);
     return choice;
   },
   generateRandomNumberFromArray(arr, player) {
-    // While Math.round(Math.random()*arr.length) is occupied keep selecting
     let index = Math.round(Math.random() * (arr.length - 1));
     while (player.shotsRecord.shotsMade.includes(index)) {
       index = Math.round(Math.random() * (arr.length - 1));
@@ -20,19 +18,18 @@ const playerMixin = (() => ({
     opponentBoard,
     choice,
     intelligentgeneratorFn,
-    randomGeneratorFn
+    randomGeneratorFn,
   ) {
     const choices = [];
     const grid = opponentBoard.grid;
     const shotsMade = player.shotsRecord.shotsMade;
     const shotsQueue = player.shotsRecord.shotsQueue;
-    // For as long as the choice does not correspond to an * then keep making a choice
     while (grid[choice] === "X") {
       const neighborIndices = [
         choice - 10,
         choice - 1,
         choice + 1,
-        choice + 10
+        choice + 10,
       ];
 
       neighborIndices.forEach(neighborIndex => {
@@ -45,56 +42,33 @@ const playerMixin = (() => ({
           shotsQueue.push(neighborIndex);
         }
       });
-      // shotsMade.push(choice);
       choices.push(choice);
       choice = intelligentgeneratorFn(
         opponentBoard.grid,
         player,
-        randomGeneratorFn
+        randomGeneratorFn,
       );
       player.turn(choice);
-      // shotsMade.push(choice);
     }
     choices.push(choice);
     player.turn(choice);
-    // shotsMade.push(choice);
-    // If the function goes inside the while loop then choices.length > 0
     return choices;
   },
 
-  // computerMakeChoice(player, ownBoard, opponentBoard, index)
+  
   computerMakeChoice(
     options,
     intelligentgeneratorFn,
     randomGeneratorFn,
-    sanitizingFn
+    sanitizingFn,
   ) {
     let choices = [];
     const { player, ownBoard, opponentBoard, index } = options;
 
-    if (
-      ownBoard.allShipsSunk("isSunk") === false &&
-      ownBoard.grid[index] === "*"
-    ) {
-      const choice = intelligentgeneratorFn(
-        opponentBoard.grid,
-        player,
-        randomGeneratorFn,
-      );
-
+    if (ownBoard.allShipsSunk("isSunk") === false && ownBoard.grid[index] === "*") {
+      const choice = intelligentgeneratorFn(opponentBoard.grid,player,randomGeneratorFn);
       player.turn(choice);
-
-      // player.shotsRecord.shotsMade.push(choice);
-
-      choices = choices.concat(
-        sanitizingFn(
-          player,
-          opponentBoard,
-          choice,
-          intelligentgeneratorFn,
-          randomGeneratorFn
-        )
-      );
+      choices = choices.concat(sanitizingFn(player,opponentBoard,choice,intelligentgeneratorFn, randomGeneratorFn));
     }
     return choices;
   }
